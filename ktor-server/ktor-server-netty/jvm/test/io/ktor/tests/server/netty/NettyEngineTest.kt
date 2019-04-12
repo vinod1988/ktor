8964eebc3aee6b945093f4aa76f2d88f852ece97
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2019-04-12 12:21:59 +0300

    Add TLS test advanced logging; Fix Jackson check

--- ktor-server/ktor-server-netty/jvm/test/io/ktor/tests/server/netty/NettyEngineTest.kt
+++ ktor-server/ktor-server-netty/jvm/test/io/ktor/tests/server/netty/NettyEngineTest.kt
@@ -2,6 +2,7 @@ package io.ktor.tests.server.netty
 
 import io.ktor.server.netty.*
 import io.ktor.server.testing.*
+import io.netty.channel.*
 
 class NettyEngineTest : EngineTestSuite<NettyApplicationEngine, NettyApplicationEngine.Configuration>(Netty) {
     init {
