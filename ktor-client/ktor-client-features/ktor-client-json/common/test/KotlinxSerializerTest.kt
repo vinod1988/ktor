8c4cc7fa00696ec1d3024bef4eb57348c20eff58
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-07-20 13:33:13 +0300

    Revert ContentType breaking changes

--- ktor-client/ktor-client-features/ktor-client-json/common/test/KotlinxSerializerTest.kt
+++ ktor-client/ktor-client-features/ktor-client-json/common/test/KotlinxSerializerTest.kt
@@ -32,9 +32,6 @@ data class GithubProfile(
 class KotlinxSerializerTest : ClientLoader() {
     @Test
     fun testRegisterCustom() {
-        // TODO: unmute when JS IR serialization is fixed https://github.com/Kotlin/kotlinx.serialization/issues/751
-        if (isKotlinJsIr) return
-
         val serializer = KotlinxSerializer()
 
         val user = User(1, "vasya")
@@ -44,9 +41,6 @@ class KotlinxSerializerTest : ClientLoader() {
 
     @Test
     fun testRegisterCustomList() {
-        // TODO: unmute when JS IR serialization is fixed https://github.com/Kotlin/kotlinx.serialization/issues/751
-        if (isKotlinJsIr) return
-
         val serializer = KotlinxSerializer()
 
         val user = User(2, "petya")
